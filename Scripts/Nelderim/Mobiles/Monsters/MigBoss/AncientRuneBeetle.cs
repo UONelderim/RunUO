8b01c0520c3137c66cae7700a12e86ea90cc3e59
kaczy <kaczyf93@gmail.com>
2022-04-29 12:02:32 +0200

    poprawka hue bossow: sarag awatar i ancient rune beetle

--- Scripts/Nelderim/Mobiles/Monsters/MigBoss/AncientRuneBeetle.cs
+++ Scripts/Nelderim/Mobiles/Monsters/MigBoss/AncientRuneBeetle.cs
@@ -19,7 +19,7 @@ namespace Server.Mobiles
 		{
 			Name = "prastary zuk runiczny";
 			Body = 244;
-            Hue = 2874;
+            Hue = 1872;
 
 			SetStr( 898, 1030 );
 			SetDex( 130, 150 );
--- Scripts/Nelderim/Mobiles/Monsters/MigBoss/SaragAwatar.cs
+++ Scripts/Nelderim/Mobiles/Monsters/MigBoss/SaragAwatar.cs
@@ -26,7 +26,7 @@ namespace Server.Mobiles
 		public SaragAwatar () : base( AIType.AI_Boss, FightMode.Closest, 12, 1, 0.2, 0.4 )
 		{
 			Body = 308;
-			Hue = 2702;
+			Hue = 1872;
 			Name = "sarag - awatar przedwiecznego nieumarłego";
 
 			BaseSoundID = 0x48D;
