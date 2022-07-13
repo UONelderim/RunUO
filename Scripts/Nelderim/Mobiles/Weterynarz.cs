899707423050c40b2ed73ab49c72341e1c21b469
Maciej Zarzycki <rapfamilly@gmail.com>
2022-07-13 17:50:02 +0200

    Weterynarz ostrzega, ze pobierze hajs

--- Scripts/Nelderim/Mobiles/Weterynarz.cs
+++ Scripts/Nelderim/Mobiles/Weterynarz.cs
@@ -75,6 +75,7 @@ namespace Server.Mobiles
 			public override void OnClick()
 			{
 				m_Wet.BeginRes( m_From );
+				m_Wet.Say( "Wskrzeszenie tego zwierzecia bedzie Cie kosztowac 20 000 centarow." ); 
 			}
 		}
 		
