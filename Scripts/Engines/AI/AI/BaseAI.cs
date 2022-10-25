b343e862e477e8bea2f7d3bdf8208d183c6bcc1b
GajuszZOrod <gajusz@orod.pl>
2022-10-25 22:23:13 +0200

    Nadanie drowim straznikom wygladu pajakow.

--- Scripts/Engines/AI/AI/BaseAI.cs
+++ Scripts/Engines/AI/AI/BaseAI.cs
@@ -2771,7 +2771,7 @@ namespace Server.Mobiles
             return (m_Mobile.FocusMob != null);
         }
 
-        private bool IsSpidersFriend(Mobile m)
+        public static bool IsSpidersFriend(Mobile m)
         {
             if (m.Race.Equals(Drow.Instance) || TownDatabase.IsCitizenOfGivenTown(m, Towns.Noamuth_Quortek))
                 return true;
