7359d075b72e4f90cab3be80e3455c40804623b0
GajuszZOrod <gajusz@orod.pl>
2022-11-13 13:13:45 +0100

    Dodan "złote runu" do golenia owiec - skladnik do krawieckiego questa na talizmany.

---
+++ Scripts/Nelderim/Engines/Quests/CraftingExperiment/GoldenWool.cs
@@ -0,0 +1,43 @@
+using System;
+using Server.Items;
+using Server.Targeting;
+
+namespace Server.Items
+{
+    public class GoldenWool : Item
+    {
+        [Constructable]
+        public GoldenWool() : this(1)
+        {
+        }
+
+        [Constructable]
+        public GoldenWool(int amount) : base(0xDF8)
+        {
+            Stackable = true;
+            Weight = 0.1;
+            Amount = amount;
+
+            Name = "Z³ote runo";
+            Hue = 1965;
+        }
+
+        public GoldenWool(Serial serial) : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0); // version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+    }
+}
