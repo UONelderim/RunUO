72650453315ce10d791e36e73726fe49bc0d6faa
Maupishon <RapFamilly@gmail.com>
2022-11-27 14:23:06 +0100

    Fix na składniki wymagane do produkcji krasnoludzkiego rumu

--- Scripts/Engines/Craft/DefCooking.cs
+++ Scripts/Engines/Craft/DefCooking.cs
@@ -155,7 +155,7 @@ namespace Server.Engines.Craft
 
 			index = AddCraft( typeof( AdmiralsHeartyRum ), "Wywary" , "Krasnoludzki Rum" , 45.0, 78.0, typeof( BaseBeverage ), 1046458, 1, 1044253 );
 			AddRes( index, typeof( WheatSheaf ), "snop siana" , 2, 1044253 );
-			AddRes( index, typeof( RedBottle ), "czerwona butelka" , 1, 1044253 );
+			AddRes( index, typeof( SmallFlask ), 1044610, 1, 1044253 );
 
 			index = AddCraft( typeof( WywarSily ), "Wywary" , "Wywar Sily" , 90.0, 150.0, typeof( BaseBeverage ), 1046458, 5, 1044253 );
 			AddRes( index, typeof( Onion ), "cebula" , 10, 1044253 );
