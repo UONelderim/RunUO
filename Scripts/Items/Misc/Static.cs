0725ccbbcdd0e468639045332391320fbae9140e
kaczy93 <kaczy93@users.noreply.github.com>
2023-02-06 12:29:48 +0100

    Add/Remove Decor

--- Scripts/Items/Misc/Static.cs
+++ Scripts/Items/Misc/Static.cs
@@ -1,99 +1,159 @@
-using System;
-using Server;
-
 namespace Server.Items
 {
-	public class Static : Item
-	{
+    public class Static : Item
+    {
         public Static() //Dupe-tastic!
-            : base( 0x80 )
+            : base(0x80)
         {
             Movable = false;
         }
 
-		[Constructable]
-		public Static( int itemID ) : base( itemID )
-		{
-			Movable = false;
-		}
+        [Constructable]
+        public Static(int itemID) : base(itemID)
+        {
+            Movable = false;
+        }
 
-		[Constructable]
-		public Static( int itemID, int count ) : this( Utility.Random( itemID, count ) )
-		{
-		}
+        [Constructable]
+        public Static(int itemID, int count) : this(Utility.Random(itemID, count))
+        {
+        }
 
-		public Static( Serial serial ) : base( serial )
-		{
-		}
+        [CommandProperty(AccessLevel.Counselor)]
+        public string DecorName
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
 
-		public override void Serialize( GenericWriter writer )
-		{
-			base.Serialize( writer );
+        [CommandProperty(AccessLevel.Counselor)]
+        public string DecorLabel
+        {
+            get { return Label1; }
+            set { Label1 = value; }
+        }
 
-			writer.Write( (int) 1 ); // version
-		}
+        [CommandProperty(AccessLevel.Counselor)]
+        public LootType DecorLootType
+        {
+            get { return LootType; }
+            set { LootType = value; }
+        }
 
-		public override void Deserialize( GenericReader reader )
-		{
-			base.Deserialize( reader );
+        [CommandProperty(AccessLevel.Counselor)]
+        public int DecorItemID
+        {
+            get { return ItemID; }
+            set { ItemID = value; }
+        }
 
-			int version = reader.ReadInt();
+        [CommandProperty(AccessLevel.Counselor)]
+        public int DecorHue
+        {
+            get { return Hue; }
+            set { Hue = value; }
+        }
 
-			if ( version == 0 && Weight == 0 )
-				Weight = -1;
-		}
-	}
+        [CommandProperty(AccessLevel.Counselor)]
+        public bool DecorMovable
+        {
+            get { return Movable; }
+            set { Movable = value; }
+        }
 
-	public class LocalizedStatic : Static
-	{
-		private int m_LabelNumber;
+        [CommandProperty(AccessLevel.Counselor)]
+        public Point3D DecorLocation
+        {
+            get { return Location; }
+            set { Location = value; }
+        }
 
-		[CommandProperty( AccessLevel.GameMaster )]
-		public int Number
-		{
-			get{ return m_LabelNumber; }
-			set{ m_LabelNumber = value; InvalidateProperties(); }
-		}
+        [CommandProperty(AccessLevel.Counselor)]
+        public double DecorWeight
+        {
+            get { return Weight; }
+            set { Weight = value; }
+        }
 
-		public override int LabelNumber{ get{ return m_LabelNumber; } }
+        public Static(Serial serial) : base(serial)
+        {
+        }
 
-		[Constructable]
-		public LocalizedStatic( int itemID ) : this( itemID, 1020000 + itemID )
-		{
-		}
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
 
-		[Constructable]
-		public LocalizedStatic( int itemID, int labelNumber ) : base( itemID )
-		{
-			m_LabelNumber = labelNumber;
-		}
+            writer.Write((int)1); // version
+        }
 
-		public LocalizedStatic( Serial serial ) : base( serial )
-		{
-		}
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
 
-		public override void Serialize( GenericWriter writer )
-		{
-			base.Serialize( writer );
+            int version = reader.ReadInt();
 
-			writer.Write( (byte) 0 ); // version
-			writer.WriteEncodedInt( (int) m_LabelNumber );
-		}
+            if (version == 0 && Weight == 0)
+                Weight = -1;
+        }
+    }
 
-		public override void Deserialize( GenericReader reader )
-		{
-			base.Deserialize( reader );
+    public class LocalizedStatic : Static
+    {
+        private int m_LabelNumber;
 
-			int version = reader.ReadByte();
+        [CommandProperty(AccessLevel.GameMaster)]
+        public int Number
+        {
+            get { return m_LabelNumber; }
+            set
+            {
+                m_LabelNumber = value;
+                InvalidateProperties();
+            }
+        }
 
-			switch ( version )
-			{
-				case 0:
-				{
-					m_LabelNumber = reader.ReadEncodedInt();
-					break;
-				}
-			}
-		}
-	}
+        public override int LabelNumber
+        {
+            get { return m_LabelNumber; }
+        }
+
+        [Constructable]
+        public LocalizedStatic(int itemID) : this(itemID, 1020000 + itemID)
+        {
+        }
+
+        [Constructable]
+        public LocalizedStatic(int itemID, int labelNumber) : base(itemID)
+        {
+            m_LabelNumber = labelNumber;
+        }
+
+        public LocalizedStatic(Serial serial) : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((byte)0); // version
+            writer.WriteEncodedInt((int)m_LabelNumber);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadByte();
+
+            switch (version)
+            {
+                case 0:
+                {
+                    m_LabelNumber = reader.ReadEncodedInt();
+                    break;
+                }
+            }
+        }
+    }
 }
---
+++ Scripts/Nelderim/Commands/Decor.cs
@@ -0,0 +1,82 @@
+﻿using Server;
+using Server.Commands;
+using Server.Commands.Generic;
+using Server.Items;
+
+namespace Nelderim.Commands
+{
+    public class Decor
+    {
+        public static void Initialize()
+        {
+            TargetCommands.Register(new AddDecor());
+            TargetCommands.Register(new RemoveDecor());
+        }
+
+        public class AddDecor : BaseCommand
+        {
+            public AddDecor()
+            {
+                AccessLevel = AccessLevel.Counselor;
+                Supports = CommandSupport.Simple;
+                Commands = new string[] { "AddDecor" };
+                ObjectTypes = ObjectTypes.All;
+                Usage = "AddDecor itemID [hue]";
+                Description = "";
+            }
+
+            public override bool ValidateArgs(BaseCommandImplementor impl, CommandEventArgs e)
+            {
+                return e.Length >= 1;
+            }
+
+            public override void Execute(CommandEventArgs e, object obj)
+            {
+                Mobile from = e.Mobile;
+
+                IPoint3D p = obj as IPoint3D;
+
+                if (p == null)
+                    return;
+
+                int itemId;
+                if (!int.TryParse(e.GetString(0), out itemId))
+                {
+                    from.SendMessage("Invalid itemid");
+                    return;
+                }
+
+                int hue = e.GetInt32(1);
+
+                Static decor = new Static(itemId);
+                decor.Hue = hue;
+                decor.MoveToWorld(new Point3D(p), from.Map);
+            }
+        }
+
+        public class RemoveDecor : BaseCommand
+        {
+            public RemoveDecor()
+            {
+                AccessLevel = AccessLevel.Counselor;
+                Supports = CommandSupport.AllNPCs | CommandSupport.AllItems;
+                Commands = new string[] { "RemoveDecor" };
+                ObjectTypes = ObjectTypes.Both;
+                Usage = "RemoveDecor";
+                Description = "";
+            }
+
+            public override void Execute(CommandEventArgs e, object obj)
+            {
+                Static p = obj as Static;
+                if (p == null)
+                {
+                    e.Mobile.SendMessage("This is not a decor");
+                    return;
+                }
+
+                p.Delete();
+            }
+        }
+    }
+}
