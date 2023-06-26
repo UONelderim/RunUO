0f5c247dc917d2b66bf13d29c43240e65515080a
Maupishon <RapFamilly@gmail.com>
2023-06-26 13:14:52 +0200

    Fix z 90 na 100

--- Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Rogue/Spells/Charm.cs
+++ Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Rogue/Spells/Charm.cs
@@ -77,7 +77,7 @@ namespace Server.ACC.CSS.Systems.Rogue
                 {
                     Caster.SendMessage("Akurat tego nie możesz kontrolować!");
                 }
-                else if (m.MinTameSkill >= 90)
+                else if (m.MinTameSkill >= 100)
                 {
                     Caster.SendMessage("Akurat tego nie możesz kontrolować!");
                 }
