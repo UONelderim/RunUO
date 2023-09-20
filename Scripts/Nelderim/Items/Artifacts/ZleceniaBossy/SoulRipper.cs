b647dd670cb5011f7c94142db2cef0b41eb237ce
Maupishon <RapFamilly@gmail.com>
2023-09-20 18:32:39 +0200

    Nie-Morderca bedzie otrzymywal dmg od hita

--- Scripts/Nelderim/Items/Artifacts/ZleceniaBossy/SoulRipper.cs
+++ Scripts/Nelderim/Items/Artifacts/ZleceniaBossy/SoulRipper.cs
@@ -1,6 +1,9 @@
 using System;
+using Server;
 using Server.Items;
 using Server.Mobiles;
+using System.Collections.Generic;
+
 
 namespace Server.Items
 {
@@ -12,25 +15,34 @@ namespace Server.Items
             Hue = 1152;
             Name = "Wysysacz Dusz";
             Weight = 5.0;
+            Slayer = SlayerName.SpidersDeath;
             WeaponAttributes.HitLeechStam = 20;
             WeaponAttributes.HitLeechMana = 20;
+            WeaponAttributes.HitLeechHits = 25;
             Attributes.WeaponSpeed = 10;
             Attributes.WeaponDamage = 60;
             AosElementDamages.Energy = 100;
         }
-        
+
         public override void AddNameProperties(ObjectPropertyList list)
         {
             base.AddNameProperties(list);
-            list.Add(1049644, "Z kazdym Twym uderzeniem, Twa dusza slabnie");
+            list.Add(1049644, "Z kazdym Twym uderzeniem, Twa dusza slabnie, jesli nie jestes morderca");
         }
 
         public SoulRipper(Serial serial) : base(serial)
         {
         }
 
-        public override int InitMinHits { get { return 50; } }
-        public override int InitMaxHits { get { return 50; } }
+        public override int InitMinHits
+        {
+            get { return 50; }
+        }
+
+        public override int InitMaxHits
+        {
+            get { return 50; }
+        }
 
 
         public override void Serialize(GenericWriter writer)
@@ -49,16 +61,18 @@ namespace Server.Items
         {
             base.OnHit(attacker, defender, damageBonus);
 
-            
-            if (attacker is PlayerMobile)
+            PlayerMobile playerMobile = attacker as PlayerMobile;
+            if (playerMobile != null)
             {
-                PlayerMobile player = (PlayerMobile)attacker;
-                player.Damage(8);
-                attacker.FixedParticles(0x3709, 10, 30, 5052, 0x480, 0, EffectLayer.LeftFoot);
-                attacker.PlaySound(0x208);
-                player.SendMessage("Twoja dusza cierpi!"); 
+                if (attacker.Kills < 5)
+                {
+                    playerMobile.Damage(8);
+                    attacker.FixedParticles(0x3709, 10, 30, 5052, 0x480, 0, EffectLayer.LeftFoot);
+                    attacker.PlaySound(0x208);
+                    playerMobile.SendMessage("Twoja dusza cierpi czarny pomiocie!");
+                }
             }
-        }
 
+        }
     }
 }
