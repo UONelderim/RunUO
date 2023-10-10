82016ca612e4f9a624b41862432879ce795f10ec
GajuszZOrod <103279149+GajuszZOrod@users.noreply.github.com>
2023-10-10 16:49:06 +0200

    Komenda [em bedzie dzialac w dowolnym punkcie mapy (nie tylko itemy/npc) (#90)
    Co-authored-by: GajuszZOrod <gajusz@orod.pl>

--- Scripts/Nelderim/Commands/Emote.cs
+++ Scripts/Nelderim/Commands/Emote.cs
@@ -1,6 +1,3 @@
-// 05.03.29  :: troyan :: polecenia przeniesione na poziom Counselor
-// 05.12.18 :: troyan :: dopasowanie do Gmoff
-
 using System;
 using Server;
 using Server.Items;
@@ -8,6 +5,8 @@ using Server.Mobiles;
 using Server.Targets;
 using Server.Network;
 using Server.Targeting;
+using Ultima;
+using Server.Commands.Generic;
 
 namespace Server.Commands
 {
@@ -18,7 +17,7 @@ namespace Server.Commands
         public static void Initialize()
         {
             m_Color = 99;
-            Register( "Em", AccessLevel.Counselor, new CommandEventHandler( emoteCommand ) );
+            TargetCommands.Register(new ShowEmote());
             Register( "SetemoteColor", AccessLevel.Counselor, new CommandEventHandler( SetemoteColorCommand ) );
         }
         
@@ -26,8 +25,61 @@ namespace Server.Commands
         {
             CommandSystem.Register( command, access, handler );
         }
-        
-		public static int Color
+
+        public class ShowEmote : BaseCommand
+        {
+            public ShowEmote()
+            {
+                AccessLevel = AccessLevel.Counselor;
+                Supports = CommandSupport.Simple;
+                Commands = new string[] { "Em" };
+                ObjectTypes = ObjectTypes.All;
+                Usage = "Em <emote text to display>";
+                Description = "Force target to emote <text>.";
+            }
+
+            public override bool ValidateArgs(BaseCommandImplementor impl, CommandEventArgs e)
+            {
+                return e.Length >= 1;
+            }
+
+            public override void Execute(CommandEventArgs e, object targeted)
+            {
+                Mobile from = e.Mobile;
+
+                string toEmote = e.ArgString;
+
+                if (targeted is Mobile)
+                {
+                    Mobile targ = (Mobile)targeted;
+
+                    if (from != targ && from.TrueAccessLevel > targ.TrueAccessLevel)
+                    {
+                        CommandLogging.WriteLine(from, "{0} {1} forcing speech on {2}", from.TrueAccessLevel, CommandLogging.Format(from), CommandLogging.Format(targ));
+                        targ.Say("*" + toEmote + "*");
+                    }
+                }
+                else if (targeted is Item)
+                {
+                    Item targ = (Item)targeted;
+
+                    targ.PublicOverheadMessage(MessageType.Regular, Say.Color, false, "*" + toEmote + "*");
+                }
+                else if (targeted is IPoint3D)
+                {
+                    Static emoteHolder = new TemporaryItem();
+                    emoteHolder.MoveToWorld(new Point3D((IPoint3D)targeted), from.Map);
+
+                    emoteHolder.PublicOverheadMessage(MessageType.Regular, Say.Color, false, "*" + toEmote + "*");
+                }
+                else
+                {
+                    from.SendMessage("Invalid target");
+                }
+            }
+        }
+
+        public static int Color
         {
             get { return m_Color; }
         }
@@ -44,46 +96,45 @@ namespace Server.Commands
                 e.Mobile.SendMessage( m_Color, "You choosed this color." );
             }
         }
-
-        [Usage( "em <text>" )]
-        [Description( "Force target to emote <text>." )]
-        public static void emoteCommand( CommandEventArgs e )
-        {
-            string toemote = e.ArgString.Trim();
-
-            if ( e.Length > 0 )
-                e.Mobile.Target =  new emoteTarget( toemote );
-            else
-                e.Mobile.SendMessage( "Format: emote \"<text>\"" );
-        }
-        
-		public class emoteTarget : Target
-        {
-            private string m_toemote;
-
-            public emoteTarget( string s ) : base ( -1, false, TargetFlags.None )
-            {
-                m_toemote = s;
-            }
-            protected override void OnTarget( Mobile from, object targeted )
-            {
-                if ( targeted is Mobile )
-                {
-                    Mobile targ = (Mobile)targeted;
-
-                    if ( from != targ && from.TrueAccessLevel > targ.TrueAccessLevel )
-                    {
-                        CommandLogging.WriteLine( from, "{0} {1} forcing speech on {2}", from.TrueAccessLevel, CommandLogging.Format( from ), CommandLogging.Format( targ ) ); 
-                        targ.Say("*" + m_toemote + "*");
-                    }
-                }
-                else if ( targeted is Item )
-                {
-                    Item targ = (Item)targeted;
-
-                    targ.PublicOverheadMessage( MessageType.Regular, Say.Color, false, "*" + m_toemote + "*");
-                }
-            }
-        }
     }
+
+    class TemporaryItem : Static
+    {
+        private Timer m_Timer;
+
+        private class ItemCleaner : Timer
+        {
+            private TemporaryItem m_Owner;
+            public ItemCleaner(TemporaryItem owner) : base(TimeSpan.FromSeconds(10))
+            {
+                m_Owner = owner;
+            }
+
+            protected override void OnTick()
+            {
+                m_Owner.Delete();
+            }
+        }
+        public TemporaryItem() : base(0x1)
+        {
+            Name = "";
+
+            m_Timer = new ItemCleaner(this);
+            m_Timer.Start();
+        }
+
+        public TemporaryItem(Serial serial) : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+        }
+    };
 }
