bdb2af448af34ac9bd28989496a761da89470ebe
Maupishon <RapFamilly@gmail.com>
2023-12-21 09:39:24 +0100

    Fajka wykrywa tyton, pobiera tyton

--- Scripts/Nelderim/Uprawy , Ogrody , Mleko , Sery , Ziola/Fajki i Tyton/Fajka.cs
+++ Scripts/Nelderim/Uprawy , Ogrody , Mleko , Sery , Ziola/Fajki i Tyton/Fajka.cs
@@ -23,21 +23,34 @@ namespace Server.Items
 
             private void InitialEffectsCallback(object state)
             {
+                int tyton = Core.AOS ? 4 : 5;
 
+                // Check if there is enough Tyton in the backpack
+                if (m_Mobile.Backpack == null || m_Mobile.Backpack.GetAmount(typeof(Tyton)) < tyton)
+                {
+                    m_Mobile.SendMessage("Za malo tytoniu, moj Panie.");
+                    return; // Exit the method if the condition is not met
+                }
+
+                // Continue with the rest of the action if there is enough Tyton
                 Effects.SendLocationParticles(EffectItem.Create(m_Mobile.Location, m_Mobile.Map, EffectItem.DefaultDuration), 0x3728, 1, 13, 9965);
-                
+
                 m_Mobile.PlaySound(0x15F);
                 m_Mobile.SendMessage("Dym tytoniowy napelnia Twe pluca");
                 m_Mobile.RevealingAction();
 
-                m_Executed = true; 
+                m_Mobile.Backpack.ConsumeUpTo(typeof(Tyton), tyton);
+
+                m_Executed = true;
             }
+
 
             protected override void OnTick()
             {
                 if (!m_Executed)
                 {
                     InitialEffectsCallback(null);
+                    Stop(); // Stop the timer after it has executed once
                 }
                 else
                 {
@@ -45,6 +58,7 @@ namespace Server.Items
                     Start();
                 }
             }
+
         }
 
         [Constructable]
@@ -58,7 +72,7 @@ namespace Server.Items
         public override void OnDoubleClick(Mobile from)
         {
             SmokeTimer timer = new SmokeTimer(from);
-            from.SendMessage("Odpalasz fajke");
+            from.SendMessage("Probujesz odpalic fajke");
         }
 
         public Fajka(Serial serial) : base(serial)
