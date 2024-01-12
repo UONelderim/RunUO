19e3f7111ae9064a15a00955fa6ddef4c9c344d1
GajuszZOrod <103279149+GajuszZOrod@users.noreply.github.com>
2024-01-12 12:50:33 +0100

    Mag przywola zywiolaka trucizny jesli umie zatruwanie. (#122)
    Co-authored-by: GajuszZOrod <gajusz@orod.pl>

---
+++ Scripts/Nelderim/Mobiles/Monsters/Magical/SummonedPoisonElemental.cs
@@ -0,0 +1,73 @@
+using System;
+using Server;
+using Server.Items;
+
+namespace Server.Mobiles
+{
+	[CorpseName( "zwloki zywiolaka trucizny" )]
+	public class SummonedPoisonElemental : BaseCreature
+	{
+		[Constructable]
+		public SummonedPoisonElemental () : base( AIType.AI_BattleMage, FightMode.Closest, 12, 1, 0.2, 0.4 )
+		{
+			Name = "zywiolak trucizny";
+			Body = 162;
+			BaseSoundID = 263;
+
+			// staty i skille w przyblizeniu wzorowane na SummonedDaemon i EnergyVortex
+
+			SetStr(200);
+			SetDex(200);
+			SetInt(100);
+
+			SetHits( 200 );
+
+			SetDamage( 14, 19 );
+
+			SetDamageType( ResistanceType.Physical, 10 );
+			SetDamageType( ResistanceType.Poison, 90 );
+
+			SetResistance(ResistanceType.Physical, 60, 70);
+			SetResistance(ResistanceType.Fire, 40, 50);
+			SetResistance(ResistanceType.Cold, 40, 50);
+			SetResistance(ResistanceType.Poison, 90);
+			SetResistance(ResistanceType.Energy, 40, 50);
+
+			SetSkill(SkillName.EvalInt, 90.1, 100.0);
+			SetSkill(SkillName.Meditation, 90.1, 100.0);
+			SetSkill(SkillName.Magery, 90.1, 100.0);
+			SetSkill(SkillName.MagicResist, 90.1, 100.0);
+			SetSkill(SkillName.Tactics, 100.0);
+			SetSkill(SkillName.Wrestling, 98.1, 99.0);
+			SetSkill(SkillName.Poisoning, 90.1, 100.0);
+
+			VirtualArmor = 70;
+
+			ControlSlots = SummonedControlSlots;
+		}
+
+		public static int SummonedControlSlots { get { return 4; } }
+
+		public override bool BleedImmune{ get{ return true; } }
+		public override Poison PoisonImmune{ get{ return Poison.Lethal; } }
+
+		public override Poison HitPoison{ get{ return Poison.Deadly; } }
+		public override double HitPoisonChance{ get{ return 0.75; } }
+
+		public SummonedPoisonElemental( Serial serial ) : base( serial )
+		{
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+			writer.Write( (int) 0 );
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+			int version = reader.ReadInt();
+		}
+	}
+}
