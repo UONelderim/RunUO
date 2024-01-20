f96f5bbf8823416ace40a4944acd1d5156078aba
GajuszZOrod <gajusz@orod.pl>
2024-01-20 16:44:24 +0100

    Helper do podmiany itemow bedzie kopiowal lokacje starego przedmiotu

--- Scripts/Nelderim/helpers/ItemExtension.cs
+++ Scripts/Nelderim/helpers/ItemExtension.cs
@@ -2,15 +2,26 @@
 
 namespace Server.Helpers
 {
-    public static class ItemExtension
-    {
-        public static void ReplaceWith(this Item oldItem, Item newItem) {
-            if (oldItem.ParentEntity is BaseContainer)
-            {
-                BaseContainer bc = (BaseContainer)oldItem.ParentEntity;
-                bc.AddItem(newItem);
-            }
-            oldItem.Delete();
-        }
-    }
+	public static class ItemExtension
+	{
+		public static void ReplaceWith(this Item oldItem, Item newItem)
+		{
+			if (oldItem.ParentEntity is BaseContainer)
+			{
+				BaseContainer bc = (BaseContainer)oldItem.ParentEntity;
+				bc.AddItem(newItem);
+			}
+			else
+			{
+				if (oldItem.Map != null && oldItem.Map != Map.Internal)
+				{
+					newItem.Map = oldItem.Map;
+					newItem.X = oldItem.X;
+					newItem.Y = oldItem.Y;
+					newItem.Z = oldItem.Z;
+				}
+			}
+			oldItem.Delete();
+		}
+	}
 }
