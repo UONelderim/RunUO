8c141bc2f03c6d5a53c36cce7ea525bb716bc94e
GajuszZOrod <103279149+GajuszZOrod@users.noreply.github.com>
2024-03-29 17:49:21 +0100

    Niebieskie skory z lodowych smokow. Czerwone skory z ognistych smokow. (#156)
    Co-authored-by: GajuszZOrod <gajusz@orod.pl>

--- Scripts/Nelderim/Mobiles/Monsters/SmokiLodowe/StaryLodowySmok.cs
+++ Scripts/Nelderim/Mobiles/Monsters/SmokiLodowe/StaryLodowySmok.cs
@@ -65,8 +65,7 @@ namespace Server.Mobiles
 		public override void GenerateLoot()
 		{
 			AddLoot( LootPack.FilthyRich, 3 );
-			// 07.01.2013 :: szczaw :: usuniecie PackGold
-			//PackGold(2000, 3000 );
+
 			AddLoot( LootPack.Gems, 3 );
 		}
 
@@ -75,7 +74,7 @@ namespace Server.Mobiles
 		public override int TreasureMapLevel{ get{ return 4; } }
 		public override int Meat{ get{ return 8; } }
 		public override int Hides{ get{ return 15; } }
-		public override HideType HideType{ get{ return HideType.Barbed; } }
+		public override HideType HideType{ get{ return HideType.Spined; } }
 		public override int Scales{ get{ return 6; } }
 		public override ScaleType ScaleType{ get{ return ( Body == 12 ? ScaleType.Yellow : ScaleType.Red ); } }
 		public override FoodType FavoriteFood{ get{ return FoodType.Meat; } }
