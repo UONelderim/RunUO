993647be56a287a055a9f962bdafc89f585ba999
Maupishon <RapFamilly@gmail.com>
2024-04-01 20:13:40 +0200

    Wyrownanie spella dajacego resa do innych spelli barda, ktore daja resy.
    Mnoznik wynosil 0.3, a w innych 5.0. Wyrownuje do pozostalych

--- Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Bard/Spells/KnightsMinneSpell.cs
+++ Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Bard/Spells/KnightsMinneSpell.cs
@@ -51,7 +51,7 @@ namespace Server.ACC.CSS.Systems.Bard
 					if ( m.BeginAction( typeof( BardKnightsMinneSpell ) ) )
 					{
 
-					TimeSpan duration = TimeSpan.FromSeconds( Caster.Skills[CastSkill].Value * 0.3 );
+					TimeSpan duration = TimeSpan.FromSeconds( Caster.Skills[CastSkill].Value * 5.0 );
 					int amount = (int)( Caster.Skills[SkillName.Musicianship].Value * .125);
 
 					m.SendMessage( "Twoja odporność fizyczna wzrasta." );
