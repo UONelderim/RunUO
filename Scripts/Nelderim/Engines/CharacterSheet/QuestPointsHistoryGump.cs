ceb4877563884e7810236f613d85ffd6cb3f0c10
GajuszZOrod <gajusz@orod.pl>
2024-05-16 22:48:02 +0200

    Widocznosc imienia mistrza gry w oknie PDK bedzie wymagac uprawnien.

--- Scripts/Nelderim/Engines/CharacterSheet/QuestPointsHistoryGump.cs
+++ Scripts/Nelderim/Engines/CharacterSheet/QuestPointsHistoryGump.cs
@@ -9,7 +9,7 @@ namespace Server.Gumps
 {
     public class QuestPointsHistoryGump : Gump
     {
-        public QuestPointsHistoryGump(PlayerMobile player) 
+        public QuestPointsHistoryGump(PlayerMobile viewer, PlayerMobile player) 
             : base(0, 0)
         {
             Dragable = true;
@@ -17,13 +17,16 @@ namespace Server.Gumps
             Resizable = false;
             Disposable = false;
 
-            AddPage(0);
+            bool gmView = viewer.AccessLevel >= AccessLevel.Counselor;
+
+				AddPage(0);
             AddBackground(0, 0, 600, 600, 5054);
             
-            AddImageTiled(10, 10, 85, 550, 1416); 
+            AddImageTiled(10, 10, gmView ? 85 : 155, 550, 1416); 
             AddImageTiled(171, 10, 60, 550, 1416); 
             AddLabel(13, 13, 0x480, "Kiedy");
-            AddLabel(98, 13, 0x480, "GM");
+            if (gmView)
+                AddLabel(98, 13, 0x480, "GM");
             AddLabel(173, 13, 0x480, "Punkty");
             AddLabel(233, 13, 0x480, "Powód");
 
@@ -31,7 +34,8 @@ namespace Server.Gumps
             foreach (var qphe in CharacterSheet.Get(player).QuestPointsHistoryEntries.Reverse())
             {
                 AddLabel(13, y, 0x480, qphe.DateTime.ToShortDateString());
-                AddLabel(98, y, 0x480, qphe.GameMaster);
+                if (gmView)
+                    AddLabel(98, y, 0x480, qphe.GameMaster);
                 AddLabel(173, y, 0x480, qphe.Change.ToString("+#;-#;0")); //Formatting to always add sign
                 AddLabelCropped(233, y, 350, 18, 0x480, qphe.Reason);
                 y += 25;
