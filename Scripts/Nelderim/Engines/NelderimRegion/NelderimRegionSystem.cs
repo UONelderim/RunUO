16b558f592dff80f20f498ca746bd3b0834a669c
kaczy <kaczyf93@gmail.com>
2024-07-25 21:05:13 +0200

    Removed XML from NelderimRegionSystem

--- Scripts/Nelderim/Engines/NelderimRegion/NelderimRegionSystem.cs
+++ Scripts/Nelderim/Engines/NelderimRegion/NelderimRegionSystem.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Xml;
 using Server.Mobiles;
-using Server.Items;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -12,7 +10,6 @@ namespace Server.Nelderim
     public class NelderimRegionSystem
     {
         internal static string BaseDir = Path.Combine(Core.BaseDirectory, "Data", "NelderimRegions");
-        private static string XmlPath = Path.Combine(BaseDir, "NelderimRegions.xml");
         private static string JsonPath = Path.Combine(BaseDir, "NelderimRegions.json");
 
         internal static Dictionary<string, NelderimRegion> NelderimRegions = new();
@@ -35,17 +32,8 @@ namespace Server.Nelderim
         private static void Load()
         {
             NelderimRegions.Clear();
-            // if (File.Exists(JsonPath))
-            // {
             var region = JsonSerializer.Deserialize<NelderimRegion>(File.ReadAllText(JsonPath), SerializerOptions);
             Add(region);
-            // }
-            // else
-            // {
-	            // LoadXml();
-	            // Save();
-            // }
-
             Console.WriteLine("NelderimRegions: Loaded.");
         }
 
@@ -62,146 +50,6 @@ namespace Server.Nelderim
 	        }
 	        region.Validate();
 	        NelderimRegions.Add(region.Name, region);
-        }
-
-        private static void LoadXml()
-        {
-            var doc = new XmlDocument();
-            doc.Load(XmlPath);
-
-            var root = doc["NelderimRegions"];
-
-            Dictionary<NelderimRegion, string> parents = new();
-            
-            foreach (XmlElement reg in root.GetElementsByTagName("region"))
-            {
-                var newRegion = new NelderimRegion();
-                newRegion.Name = reg.GetAttribute("name");
-                var parent = reg.GetAttribute("parent");
-                if (parent != "")
-	                parents[newRegion] = parent;
-
-                var oreveins = reg.GetElementsByTagName("oreveins");
-
-                if (oreveins.Count > 0)
-                {
-	                newRegion.Resources = new Dictionary<CraftResource, double>();
-                    var pop = oreveins.Item(0) as XmlElement;
-
-                    for (CraftResource res = CraftResource.Iron; res <= CraftResource.Valorite; res++)
-                    {
-                        newRegion.Resources[res] = XmlConvert.ToDouble(pop.GetAttribute(res.ToString()));
-                    }
-                }
-
-                var woodveins = reg.GetElementsByTagName("woodveins");
-                if (woodveins.Count > 0)
-                {
-	                newRegion.Resources ??= new Dictionary<CraftResource, double>();
-                    var pop = woodveins.Item(0) as XmlElement;
-                    for (CraftResource res = CraftResource.RegularWood; res <= CraftResource.Frostwood; res++)
-                    {
-                        newRegion.Resources[res] = XmlConvert.ToDouble(pop.GetAttribute(res.ToString()));
-                    }
-                }
-
-                //Banned followers are deducted from banned schools
-                var bannedSchools = reg.GetElementsByTagName("bannedschools");
-
-                if (bannedSchools.Count > 0)
-                {
-	                newRegion.BannedSchools = new NelderimRegionSchools();
-                    var pop = bannedSchools.Item(0) as XmlElement;
-
-                    newRegion.BannedSchools.Magery = XmlConvert.ToInt32(pop.GetAttribute("magery")) == 1;
-                    newRegion.BannedSchools.Chivalry = XmlConvert.ToInt32(pop.GetAttribute("chivalry")) == 1;
-                    newRegion.BannedSchools.Necromancy = XmlConvert.ToInt32(pop.GetAttribute("necromancy")) == 1;
-                    newRegion.BannedSchools.Spellweaving = XmlConvert.ToInt32(pop.GetAttribute("druidism")) == 1;
-                    
-                    if(newRegion.BannedSchools.Magery == false && 
-                       newRegion.BannedSchools.Chivalry == false && 
-                       newRegion.BannedSchools.Necromancy == false && 
-                       newRegion.BannedSchools.Spellweaving == false)
-					{
-	                    newRegion.BannedSchools = null;
-					}
-                }
-
-                var intoleranceTag = reg.GetElementsByTagName("intolerance");
-
-                if (intoleranceTag.Count > 0)
-                {
-	                newRegion.Intolerance = new Dictionary<string, double>();
-                    var pop = intoleranceTag.Item(0) as XmlElement;
-		
-                    foreach (var race in Race.AllRaces)
-                    {
-                        string attr = pop.GetAttribute(race.Name);
-                        if(attr == "" || attr == "0")
-	                        continue;
-                        
-                        newRegion.Intolerance[race.Name] = XmlConvert.ToDouble(attr) / 100f;
-                    }
-                }
-
-                var races = reg.GetElementsByTagName("races");
-
-                if (races.Count > 0)
-                {
-	                newRegion.Population = new Dictionary<string, double>();
-                    var pop = races.Item(0) as XmlElement;
-
-                    foreach (var race in Race.AllRaces)
-                    {
-                        string attr = pop.GetAttribute(race.Name);
-                        if(attr == "" || attr == "0")
-	                        continue;
-                        
-						newRegion.Population[race.Name] = XmlConvert.ToDouble(attr) / 100f;
-                    }
-                }
-
-                var g = reg.GetElementsByTagName("guards").Item(0) as XmlElement;
-
-                if(g != null)
-                {
-	                newRegion.Guards = new Dictionary<GuardType, NelderimRegionGuard>();
-	                foreach (XmlElement guard in g.GetElementsByTagName("guard"))
-	                {
-		                var type = (GuardType)XmlConvert.ToInt32(guard.GetAttribute("type"));
-		                var guardDef = new NelderimRegionGuard();
-		                guardDef.Name = guard.GetAttribute("file");
-		                guardDef.Female = XmlConvert.ToDouble(guard.GetAttribute("female"));
-
-		                var guardRaces = guard.GetElementsByTagName("races").Item(0) as XmlElement;
-
-		                foreach (var race in Race.AllRaces)
-		                {
-			                guardDef.Population ??= new Dictionary<string, double>();
-			                string attr = guardRaces.GetAttribute(race.Name);
-			                if(attr == "" || attr == "0")
-				                continue;
-			                
-			                guardDef.Population[race.Name] = XmlConvert.ToDouble(attr) / 100f;
-		                }
-
-		                newRegion.Guards[type] = guardDef;
-	                }
-                }
-
-                newRegion.Validate();
-                NelderimRegions.Add(newRegion.Name, newRegion);
-            }
-            
-            foreach (var keyValuePair in parents)
-            {
-	            var region = keyValuePair.Key;
-	            var parent =  NelderimRegions[keyValuePair.Value];
-	            region.Parent = parent;
-	            parent.Regions ??= new List<NelderimRegion>();
-	            parent.Regions.Add(region);
-
-            }
         }
 
         private static void Save()
