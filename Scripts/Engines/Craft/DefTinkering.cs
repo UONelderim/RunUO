5caff279ebbdc42de10bec7c0142cd790697a3da
Maupishon <rapfamilly@gmail.com>
2024-08-12 14:06:50 +0200

    PowerGeneratorKey can be crafted by tinkers

--- Scripts/Engines/Craft/DefTinkering.cs
+++ Scripts/Engines/Craft/DefTinkering.cs
@@ -372,6 +372,10 @@ namespace Server.Engines.Craft
 			AddRes( index, typeof( BarrelLidRC ), 1044251, 1, 1044253 );
 			AddRes( index, typeof( BarrelTap ), 1044252, 1, 1044253 );
 			
+			index = AddCraft(typeof(PowerGeneratorKey), 1044051, "Klucz deszyfrujacy", 80.0, 123.0, typeof(GoldIngot), "sztaba zlota", 10, "Nie masz wystarczajacej ilosci sztab zlota");
+			AddRes(index, typeof(Lockpick), 1025371, 1, "Nie masz wystarczajacej ilosci wytrychow");
+			AddRes(index, typeof(VolcanicAsh), "pyl wulkaniczny", 10, "Nie masz wystarczajacej ilosci pylu wulkanicznego");
+			AddRes(index, typeof(GazerEye), "oko gazera", 10, "Nie masz wystarczajacej ilosci oczu gazera");
 			
 			#endregion
 
