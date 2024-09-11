4edb2c70a0f2a03b51af9e39b7161cb9c1b9aa9c
Maupishon <rapfamilly@gmail.com>
2024-09-11 14:19:02 +0200

    Fixed timers in gluttonousblade.cs

--- Scripts/Nelderim/Items/Artifacts/ZleceniaBossy/gluttonousblade.cs
+++ Scripts/Nelderim/Items/Artifacts/ZleceniaBossy/gluttonousblade.cs
@@ -11,34 +11,23 @@ namespace Server.Items
 {
     public class FeedTarget : Target
     {
-        private gluttonousblade m_Item;
+        private GluttonousBlade m_Item;
 
-        public FeedTarget(gluttonousblade item)
+        public FeedTarget(GluttonousBlade item)
             : base(1, false, TargetFlags.None)
         {
-            if (item == null)
-            {
-                Console.WriteLine("Item is null!"); // You can replace this with your desired error handling logic
-                return; // Exit the constructor without setting m_Item
-            }
-
-            m_Item = item;
+            m_Item = item ?? throw new ArgumentNullException(nameof(item));
         }
 
         protected override void OnTarget(Mobile from, object target)
         {
-            if (target is Gold)
+            if (target is Gold gold)
             {
-                gluttonousblade.m_HungerT = TimeSpan.Zero;
-
-                if (gluttonousblade.m_HungerT == TimeSpan.Zero)
-                {
-                    gluttonousblade.m_HungerT = TimeSpan.FromMinutes(((Gold)target).Amount / 100.0 ); // how many minutes to add depending on Gold dropped
-                    from.SendMessage("bron konsumuje {0} centarow", ((Gold)target).Amount);
-                    from.PlaySound(1073);
-                    m_Item.StartTimer();
-                    ((Item)target).Delete();
-                }
+                m_Item.TimeLeft = TimeSpan.FromMinutes(gold.Amount / 100.0);
+                from.SendMessage($"Bron konsumuje {gold.Amount} centarow");
+                from.PlaySound(1073);
+                m_Item.StartTimer();
+                gold.Delete();
             }
             else
             {
@@ -48,13 +37,10 @@ namespace Server.Items
         }
     }
 
-    public class gluttonousblade : Kryss
+    public class GluttonousBlade : Kryss
     {
-        private CustomTimer m_Timer;
-        public static TimeSpan m_HungerT;
-
-        private object timerLock = new object(); // Add this field to handle synchronization
-        private int timerCallbackCounter = 0; // Add this field to distinguish different callback invocations
+        private Timer m_Timer;
+        private TimeSpan m_TimeLeft;
 
         public override int InitMinHits { get { return 100; } }
         public override int InitMaxHits { get { return 100; } }
@@ -64,18 +50,18 @@ namespace Server.Items
         public override int AosSpeed { get { return 60; } }
 
         [Constructable]
-        public gluttonousblade()
+        public GluttonousBlade()
             : base()
         {
             Name = "Krys Przekletego Glodu";
             Hue = 1287;
             LootType = LootType.Cursed;
 
-            m_HungerT = TimeSpan.FromMinutes(10); // set starting minutes until it resets back to the basic weapon
+            m_TimeLeft = TimeSpan.FromMinutes(10);
             StartTimer();
         }
 
-        public gluttonousblade(Serial serial)
+        public GluttonousBlade(Serial serial)
             : base(serial)
         {
         }
@@ -83,10 +69,10 @@ namespace Server.Items
         [CommandProperty(AccessLevel.GameMaster)]
         public TimeSpan TimeLeft
         {
-            get { return m_HungerT; }
+            get { return m_TimeLeft; }
             set
             {
-                m_HungerT = value;
+                m_TimeLeft = value;
                 InvalidateProperties();
             }
         }
@@ -97,50 +83,35 @@ namespace Server.Items
 
             if (m_Timer == null)
             {
-                list.Add(1114057, "[Jestem głodny!]"); 
+                list.Add(1114057, "[Jestem głodny!]");
                 return;
             }
-            
 
-            TimeSpan t = m_HungerT;
-            int hours = t.Hours;
-            int minutes = t.Minutes;
+            int hours = (int)m_TimeLeft.TotalHours;
+            int minutes = m_TimeLeft.Minutes;
 
-            list.Add( $"[Bede jeszcze najedzony: {hours} i {minutes} godzin ziemi Nelderim]");
-
-        }
-
-        private void InvalidatePropertiesCallback()
-        {
-            InvalidateProperties();
+            list.Add($"[Bede jeszcze najedzony: {hours} i {minutes} godzin ziemi Nelderim]");
         }
 
         public override void OnHit(Mobile attacker, Mobile defender, double damageBonus)
         {
-            if (defender is BaseCreature)
+            if (defender is BaseCreature && 0.03 > Utility.RandomDouble())
             {
-                if (0.03 > Utility.RandomDouble())
-                {
-                    defender.Hits -= 50;
-                    defender.PlaySound(1067);
-                    attacker.SendMessage("Klątwa ostrza zaczyna się uaktywniać.");
-                    attacker.Hits -= 15;
-                    attacker.Mana -= 15;
-                    attacker.Stam -= 15;
-                    attacker.Say("DAJ MI ZŁOTA!!!");
-                }
+                defender.Hits -= 50;
+                defender.PlaySound(1067);
+                attacker.SendMessage("Klątwa ostrza zaczyna się uaktywniać.");
+                attacker.Hits -= 15;
+                attacker.Mana -= 15;
+                attacker.Stam -= 15;
+                attacker.Say("DAJ MI ZŁOTA!!!");
             }
             base.OnHit(attacker, defender, damageBonus);
         }
 
         public virtual void StartTimer()
         {
-            if (m_Timer != null)
-            {
-                return;
-            }
+            StopTimer();
 
-            //Attributes to add once "fed"
             Name = "Krys Przekletego Glodu";
             Attributes.AttackChance = 10;
             Attributes.WeaponDamage = 45;
@@ -154,34 +125,18 @@ namespace Server.Items
             Attributes.BonusInt = 3;
             Hue = 2675;
 
-            // Use CustomTimer instead of Timer
-            m_Timer = new CustomTimer(this);
-            m_Timer.Start();
-        }
-
-        private void SliceCallback(object state)
-        {
-            lock (timerLock)
-            {
-                int callbackCounter = (int)state;
-
-                if (callbackCounter != timerCallbackCounter)
-                    return;
-
-                Slice();
-            }
+            m_Timer = Timer.DelayCall(TimeSpan.FromSeconds(1.0), TimeSpan.FromSeconds(1.0), new TimerCallback(Slice));
         }
 
         public virtual void Slice()
         {
-            InvalidateProperties();
-
-            if (m_HungerT <= TimeSpan.Zero)
+            if (m_TimeLeft <= TimeSpan.Zero)
             {
                 Delete();
                 return;
             }
-            m_HungerT -= TimeSpan.FromSeconds(1);
+            m_TimeLeft -= TimeSpan.FromSeconds(1);
+            InvalidateProperties();
         }
 
         public virtual void Delete()
@@ -190,9 +145,8 @@ namespace Server.Items
             Effects.PlaySound(Location, Map, 0x201);
 
             StopTimer();
-            m_HungerT = TimeSpan.Zero;
+            m_TimeLeft = TimeSpan.Zero;
 
-            // Remove the weapon attributes
             Name = "Nienazarty kryss";
             Attributes.AttackChance = 0;
             Attributes.WeaponDamage = 0;
@@ -210,9 +164,10 @@ namespace Server.Items
         public virtual void StopTimer()
         {
             if (m_Timer != null)
+            {
                 m_Timer.Stop();
-
-            m_Timer = null;
+                m_Timer = null;
+            }
         }
 
         public override void OnDoubleClick(Mobile from)
@@ -242,24 +197,6 @@ namespace Server.Items
             int version = reader.ReadInt();
             TimeLeft = reader.ReadTimeSpan();
             StartTimer();
-        }
-
-        // Custom Timer class for handling the callback
-        private class CustomTimer : Timer
-        {
-            private gluttonousblade m_Owner;
-
-            public CustomTimer(gluttonousblade owner)
-                : base(TimeSpan.FromSeconds(1.0), TimeSpan.FromSeconds(1.0))
-            {
-                m_Owner = owner;
-            }
-
-            protected override void OnTick()
-            {
-                m_Owner.SliceCallback(m_Owner.timerCallbackCounter);
-                m_Owner.timerCallbackCounter++;
-            }
         }
     }
 }
