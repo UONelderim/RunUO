3e24ca489b0a994c44dd6df74f7dd6281560a6d5
GajuszZOrod <103279149+GajuszZOrod@users.noreply.github.com>
2024-10-10 09:06:46 +0200

    Dodanie surowcow do spawnera stealable artifacts (#211)
    Co-authored-by: GajuszZOrod <gajusz@orod.pl>

--- Scripts/Nelderim/Items/Artifacts/OriginalModified/DecorationArtifacts/StealableArtifactsSpawner.cs
+++ Scripts/Nelderim/Items/Artifacts/OriginalModified/DecorationArtifacts/StealableArtifactsSpawner.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using Server;
 using Server.Commands;
+using Server.Engines.XmlSpawner2;
+using Ultima;
 
 namespace Server.Items
 {
@@ -9,64 +11,166 @@ namespace Server.Items
 	{
 		public class StealableEntry
 		{
+			private Spot[] m_Spots;
+			private StealableType[] m_Types;
+
 			private Map m_Map;
 			private Point3D m_Location;
+			private Type m_Type;
+
 			private int m_MinDelay;
 			private int m_MaxDelay;
-			private Type m_Type;
 			private int m_Hue;
 
-			public Map Map{ get{ return m_Map; } }
-			public Point3D Location{ get{ return m_Location; } }
-			public int MinDelay{ get{ return m_MinDelay; } }
-			public int MaxDelay{ get{ return m_MaxDelay; } }
-			public Type Type{ get{ return m_Type; } }
-			public int Hue{ get{ return m_Hue; } }
+			public Spot[] Locations { get { return m_Spots; } }
+			public StealableType[] Types { get { return m_Types; } }
 
-			public StealableEntry( Map map, Point3D location, int minDelay, int maxDelay, Type type ) : this( map, location, minDelay, maxDelay, type, 0 )
+			public Map Map { get { return m_Map; } }
+			public Point3D Spot { get { return m_Location; } }
+			public Type Type { get { return m_Type; } }
+
+			public int MinDelay { get { return m_MinDelay; } }
+			public int MaxDelay { get { return m_MaxDelay; } }
+			public int Hue { get { return m_Hue; } }
+
+			public StealableEntry(Spot[] spots, int minDelay, int maxDelay, StealableType[] types)
+			{
+				m_Spots = spots;
+				m_Types = types;
+
+				m_MinDelay = minDelay;
+				m_MaxDelay = maxDelay;
+			}
+
+			public StealableEntry(Map map, Point3D location, int minDelay, int maxDelay, Type type) : this(map, location, minDelay, maxDelay, type, 0)
 			{
 			}
 
-			public StealableEntry( Map map, Point3D location, int minDelay, int maxDelay, Type type, int hue )
+			public StealableEntry(Map map, Point3D location, int minDelay, int maxDelay, Type type, int hue)
 			{
 				m_Map = map;
 				m_Location = location;
+				m_Type = type;
+
 				m_MinDelay = minDelay;
 				m_MaxDelay = maxDelay;
-				m_Type = type;
 				m_Hue = hue;
 			}
 
 			public Item CreateInstance()
 			{
-				Item item = (Item) Activator.CreateInstance( m_Type );
+				Type type = m_Type;
+				int amount = 1;
+				if (m_Types != null && m_Types.Length > 0)
+				{
+					var typeInfo = Utility.RandomList(m_Types);
+					type = typeInfo.Type;
+					amount = typeInfo.Amount;
+				}
 
-				if ( m_Hue > 0 )
+				Map map = m_Map;
+				Point3D location = m_Location;
+				if (m_Spots != null && m_Spots.Length > 0)
+				{
+					var spot = Utility.RandomList(m_Spots);
+					map = spot.Map;
+					location = spot.Point;
+				}
+
+				Item item = (Item)Activator.CreateInstance(type);
+
+				if (m_Hue > 0)
 					item.Hue = m_Hue;
 
+				item.Amount = amount;
+
 				item.Movable = false;
-				item.MoveToWorld( this.Location, this.Map );
+				item.MoveToWorld(location, map);
 
 				return item;
 			}
 		}
 
+		public class Spot
+		{
+			private Map m_Map;
+			private Point3D m_Point;
+			public Map Map { get { return m_Map; } }
+			public Point3D Point { get { return m_Point; } }
+
+			public Spot(Map map, Point3D point)
+			{
+				m_Map = map;
+				m_Point = point;
+			}
+		}
+
+		public class StealableType
+		{
+			private Type m_Type;
+			private int m_Amount;
+			public Type Type { get { return m_Type; } }
+			public int Amount { get { return m_Amount; } }
+
+			public StealableType(Type type, int amount = 1)
+			{
+				m_Type = type;
+				m_Amount = amount;
+			}
+
+		}
+
+		private static Spot[] m_DungeonsA = new Spot[]
+		{
+			new Spot(Map.Felucca, new Point3D(2660, 1939, 0)),	// minotaury
+			new Spot(Map.Felucca, new Point3D(5333, 1149, 0)),	// wulkan
+			new Spot(Map.Felucca, new Point3D(6038, 81, 4)),	// garth
+			new Spot(Map.Felucca, new Point3D(665, 1476, 45)),	// alcala
+			new Spot(Map.Felucca, new Point3D(1668, 994, 20)),	// elbrind
+			new Spot(Map.Felucca, new Point3D(5721, 474, 4)),	// barad-dur
+			new Spot(Map.Felucca, new Point3D(5647, 1835, 0)),	// hall torech
+
+			//new Spot(Map.Felucca, new Point3D(6915, 62, 0)),	// pokraczna bestia
+			//new Spot(Map.Felucca, new Point3D(6635, 77, 1)),	// blyskow
+			new Spot(Map.Malas, new Point3D(131, 1174, 0)),	// pokraczna bestia
+			new Spot(Map.Malas, new Point3D(259, 765, 1)),	// blyskow
+
+			//new Spot(Map.Felucca, new Point3D(6197, 46, -1)),	// elghinn
+			//new Spot(Map.Malas, new Point3D(53, 46, -1)),	// elghinn
+		};
+
+		private static StealableType[] m_ResourcesA = new StealableType[]
+		{
+			new StealableType(typeof(PowderOfTranslocation), 10),
+			new StealableType(typeof(PowerScrollPowder), 80),
+			new StealableType(typeof(ArtefaktowyPyl), 100),
+			new StealableType(typeof(PowderForTinkering)),
+			new StealableType(typeof(PowderForWood)),
+			new StealableType(typeof(BasePigment)),
+			new StealableType(typeof(ValoriteIngot), 250),
+			new StealableType(typeof(VeriteIngot), 300),
+			new StealableType(typeof(BowstringFiresteed), 8),
+			new StealableType(typeof(BowstringUnicorn), 8),
+			new StealableType(typeof(BowstringNightmare), 8),
+			new StealableType(typeof(BowstringKirin), 8),
+		};
+
+
 		private static StealableEntry[] m_Entries = new StealableEntry[]
 			{
-				// Customowe Arty - Felucca & Malas Dungi
-				
-				 new StealableEntry( Map.Felucca, new Point3D( 5302,  1648, 0 ), 18432, 27648, typeof( PrzekleteFeyLeggings ) ), //Saew
+				new StealableEntry( m_DungeonsA, 60*24 * 9, 60*24 * 11, m_ResourcesA ),   // co ~10 dni
+				new StealableEntry( m_DungeonsA, 60*24 * 19, 60*24 * 21, m_ResourcesA ),  // co ~20 dni
+				new StealableEntry( m_DungeonsA, 60*24 * 29, 60*24 * 31, m_ResourcesA ),  // co ~30 dni
 
-				 new StealableEntry( Map.Felucca, new Point3D( 1547,  798, 0 ), 18432, 27648, typeof( PrzekletySoulSeeker ) ), // Elbrind
-	
+				// Customowe Arty - Felucca & Malas Dungi				
+				new StealableEntry( Map.Felucca, new Point3D( 5302,  1648, 0 ), 18432, 27648, typeof( PrzekleteFeyLeggings ) ), //Saew
+				new StealableEntry( Map.Felucca, new Point3D( 1547,  798, 0 ), 18432, 27648, typeof( PrzekletySoulSeeker ) ), // Elbrind
 				new StealableEntry( Map.Felucca, new Point3D( 5702,  418, 4 ), 18432, 27648, typeof( PrzekletyMieczeAmrIbnLuhajj ) ), // Barad
-
-				new StealableEntry( Map.Felucca, new Point3D( 665,  1476, 45 ), 18432, 27648, typeof( PrzekletaVioletCourage ) ), // Alcala
-
-				new StealableEntry( Map.Malas, new Point3D( 258,  772, 1 ), 18432, 27648, typeof( PrzekletyArcticDeathDealer) ),  // Jaskina Blyskow
-
+				new StealableEntry( Map.Felucca, new Point3D( 5614,  683, 5 ), 18432, 27648, typeof( PrzekletaVioletCourage ) ), // Ophidianie
+				new StealableEntry( Map.Malas, new Point3D( 5807,  211, 50 ), 18432, 27648, typeof( PrzekletyArcticDeathDealer) ),  // Ulnhyr Orbben
 				new StealableEntry( Map.Felucca, new Point3D( 6004, 2551, 0 ), 18432, 27648, typeof( PrzekleteSongWovenMantle ) ), // Podmrok mrowki
-				
+
+
 				// Doom - Artifact rarity 1
 				new StealableEntry( Map.Malas, new Point3D( 317,  56, -1 ), 72, 108, typeof( RockArtifact ) ), // doom
 				new StealableEntry( Map.Malas, new Point3D( 360,  31,  8 ), 72, 108, typeof( SkullCandleArtifact ) ), // doom
@@ -113,7 +217,7 @@ namespace Server.Items
 				new StealableEntry( Map.Felucca, new Point3D(  5405,  648,  -25 ), 144, 216, typeof( Basket5WestArtifact ) ), // Kanały Tasandora
 				// Yomotsu Mines - Artifact rarity 3
 				new StealableEntry( Map.Felucca, new Point3D( 5389,   731, -25 ), 288, 432, typeof( Urn1Artifact ) ), // Kanały Tasandora
-				new StealableEntry( Map.Felucca, new Point3D(  5494,  699, -26 ), 288, 432, typeof( Urn2Artifact ) ), 
+				new StealableEntry( Map.Felucca, new Point3D(  5494,  699, -26 ), 288, 432, typeof( Urn2Artifact ) ),
 				new StealableEntry( Map.Felucca, new Point3D( 5299,  810, 4 ), 288, 432, typeof( Sculpture1Artifact ) ), // Hurengrav Lochy
 				new StealableEntry( Map.Felucca, new Point3D( 1523,  818, 0 ), 288, 432, typeof( Sculpture2Artifact ) ), // Elbrind
 				new StealableEntry( Map.Felucca, new Point3D( 1545,  797, 40 ), 288, 432, typeof( TeapotNorthArtifact ) ), // Elbrind
@@ -159,7 +263,7 @@ namespace Server.Items
 				// Fan Dancer's Dojo - Artifact rarity 8
 				new StealableEntry( Map.Felucca, new Point3D(  5379, 726, -25 ), 9216, 13824, typeof( DolphinLeftArtifact ) ), // Kanaly Tasandora
 				new StealableEntry( Map.Felucca, new Point3D( 5384, 328, 5 ), 9216, 13824, typeof( DolphinRightArtifact ) ), // Cmentarz Tasandora
-				new StealableEntry( Map.Felucca, new Point3D(  5238, 361	,  0 ), 9216, 13824, typeof( SwordDisplay3SouthArtifact ) ), // Kopalnia Tasandora
+				new StealableEntry( Map.Felucca, new Point3D(  5238, 361    ,  0 ), 9216, 13824, typeof( SwordDisplay3SouthArtifact ) ), // Kopalnia Tasandora
 				new StealableEntry( Map.Felucca, new Point3D( 5321, 56, 0 ), 9216, 13824, typeof( SwordDisplay3EastArtifact ) ), // Komnaty Bialego Wilka
 				new StealableEntry( Map.Malas, new Point3D( 162, 647, -1 ), 9216, 13824, typeof( SwordDisplay4WestArtifact ) ),
 				new StealableEntry( Map.Malas, new Point3D( 124, 624,  0 ), 9216, 13824, typeof( Painting5NorthArtifact ) ),
@@ -173,18 +277,18 @@ namespace Server.Items
 				new StealableEntry( Map.Malas, new Point3D( 178, 629, -1 ), 18432, 27648, typeof( ManStatuetteEastArtifact ) )
 			};
 
-		public static StealableEntry[] Entries{ get{ return m_Entries; } }
+		public static StealableEntry[] Entries { get { return m_Entries; } }
 
 		private static Type[] m_TypesOfEntries = null;
 		public static Type[] TypesOfEntires
 		{
 			get
 			{
-				if( m_TypesOfEntries == null )
+				if (m_TypesOfEntries == null)
 				{
 					m_TypesOfEntries = new Type[m_Entries.Length];
 
-					for( int i = 0; i < m_Entries.Length; i++ )
+					for (int i = 0; i < m_Entries.Length; i++)
 						m_TypesOfEntries[i] = m_Entries[i].Type;
 				}
 
@@ -194,50 +298,50 @@ namespace Server.Items
 
 		private static StealableArtifactsSpawner m_Instance;
 
-		public static StealableArtifactsSpawner Instance{ get{ return m_Instance; } }
+		public static StealableArtifactsSpawner Instance { get { return m_Instance; } }
 
 		private static int GetLampPostHue()
 		{
-			if ( 0.9 > Utility.RandomDouble() )
+			if (0.9 > Utility.RandomDouble())
 				return 0;
 
-			return Utility.RandomList( 0x455, 0x47E, 0x482, 0x486, 0x48F, 0x4F2, 0x58C, 0x66C );
+			return Utility.RandomList(0x455, 0x47E, 0x482, 0x486, 0x48F, 0x4F2, 0x58C, 0x66C);
 		}
 
 
 		public static void Initialize()
 		{
-			CommandSystem.Register( "GenStealArties", AccessLevel.Administrator, new CommandEventHandler( GenStealArties_OnCommand ) );
-			CommandSystem.Register( "RemoveStealArties", AccessLevel.Administrator, new CommandEventHandler( RemoveStealArties_OnCommand ) );
+			CommandSystem.Register("GenStealArties", AccessLevel.Administrator, new CommandEventHandler(GenStealArties_OnCommand));
+			CommandSystem.Register("RemoveStealArties", AccessLevel.Administrator, new CommandEventHandler(RemoveStealArties_OnCommand));
 		}
 
-		[Usage( "GenStealArties" )]
-		[Description( "Generates the stealable artifacts spawner." )]
-		private static void GenStealArties_OnCommand( CommandEventArgs args )
+		[Usage("GenStealArties")]
+		[Description("Generates the stealable artifacts spawner.")]
+		private static void GenStealArties_OnCommand(CommandEventArgs args)
 		{
 			Mobile from = args.Mobile;
 
-			if ( Create() )
-				from.SendMessage( "Stealable artifacts spawner generated." );
+			if (Create())
+				from.SendMessage("Stealable artifacts spawner generated.");
 			else
-				from.SendMessage( "Stealable artifacts spawner already present." );
+				from.SendMessage("Stealable artifacts spawner already present.");
 		}
 
-		[Usage( "RemoveStealArties" )]
-		[Description( "Removes the stealable artifacts spawner and every not yet stolen stealable artifacts." )]
-		private static void RemoveStealArties_OnCommand( CommandEventArgs args )
+		[Usage("RemoveStealArties")]
+		[Description("Removes the stealable artifacts spawner and every not yet stolen stealable artifacts.")]
+		private static void RemoveStealArties_OnCommand(CommandEventArgs args)
 		{
 			Mobile from = args.Mobile;
 
-			if ( Remove() )
-				from.SendMessage( "Stealable artifacts spawner removed." );
+			if (Remove())
+				from.SendMessage("Stealable artifacts spawner removed.");
 			else
-				from.SendMessage( "Stealable artifacts spawner not present." );
+				from.SendMessage("Stealable artifacts spawner not present.");
 		}
 
 		public static bool Create()
 		{
-			if ( m_Instance != null && !m_Instance.Deleted )
+			if (m_Instance != null && !m_Instance.Deleted)
 				return false;
 
 			m_Instance = new StealableArtifactsSpawner();
@@ -246,7 +350,7 @@ namespace Server.Items
 
 		public static bool Remove()
 		{
-			if ( m_Instance == null )
+			if (m_Instance == null)
 				return false;
 
 			m_Instance.Delete();
@@ -254,12 +358,12 @@ namespace Server.Items
 			return true;
 		}
 
-		public static StealableInstance GetStealableInstance( Item item )
+		public static StealableInstance GetStealableInstance(Item item)
 		{
-			if ( Instance == null )
+			if (Instance == null)
 				return null;
 
-			return (StealableInstance) Instance.m_Table[item];
+			return (StealableInstance)Instance.m_Table[item];
 		}
 
 
@@ -269,25 +373,25 @@ namespace Server.Items
 			private Item m_Item;
 			private DateTime m_NextRespawn;
 
-			public StealableEntry Entry{ get{ return m_Entry; } }
+			public StealableEntry Entry { get { return m_Entry; } }
 
 			public Item Item
 			{
-				get{ return m_Item; }
+				get { return m_Item; }
 				set
 				{
-					if ( m_Item != null && value == null )
+					if (m_Item != null && value == null)
 					{
-						int delay = Utility.RandomMinMax( this.Entry.MinDelay, this.Entry.MaxDelay );
-						this.NextRespawn = DateTime.Now + TimeSpan.FromMinutes( delay );
+						int delay = Utility.RandomMinMax(this.Entry.MinDelay, this.Entry.MaxDelay);
+						this.NextRespawn = DateTime.Now + TimeSpan.FromMinutes(delay);
 					}
 
-					if ( Instance != null )
+					if (Instance != null)
 					{
-						if ( m_Item != null	)
-							Instance.m_Table.Remove( m_Item );
+						if (m_Item != null)
+							Instance.m_Table.Remove(m_Item);
 
-						if ( value != null )
+						if (value != null)
 							Instance.m_Table[value] = this;
 					}
 
@@ -297,15 +401,15 @@ namespace Server.Items
 
 			public DateTime NextRespawn
 			{
-				get{ return m_NextRespawn; }
-				set{ m_NextRespawn = value; }
+				get { return m_NextRespawn; }
+				set { m_NextRespawn = value; }
 			}
 
-			public StealableInstance( StealableEntry entry ) : this( entry, null, DateTime.Now )
+			public StealableInstance(StealableEntry entry) : this(entry, null, DateTime.Now)
 			{
 			}
 
-			public StealableInstance( StealableEntry entry, Item item, DateTime nextRespawn )
+			public StealableInstance(StealableEntry entry, Item item, DateTime nextRespawn)
 			{
 				m_Item = item;
 				m_NextRespawn = nextRespawn;
@@ -314,10 +418,10 @@ namespace Server.Items
 
 			public void CheckRespawn()
 			{
-				if ( this.Item != null && ( this.Item.Deleted || this.Item.Movable || this.Item.Parent != null ) )
+				if (this.Item != null && (this.Item.Deleted || this.Item.Movable || this.Item.Parent != null))
 					this.Item = null;
 
-				if ( this.Item == null && DateTime.Now >= this.NextRespawn )
+				if (this.Item == null && DateTime.Now >= this.NextRespawn)
 				{
 					this.Item = this.Entry.CreateInstance();
 				}
@@ -333,34 +437,34 @@ namespace Server.Items
 			get { return "Stealable Artifacts Spawner - Internal"; }
 		}
 
-		private StealableArtifactsSpawner() : base( 1 )
+		private StealableArtifactsSpawner() : base(1)
 		{
 			Movable = false;
 
 			m_Artifacts = new StealableInstance[m_Entries.Length];
-			m_Table = new Hashtable( m_Entries.Length );
+			m_Table = new Hashtable(m_Entries.Length);
 
-			for ( int i = 0; i < m_Entries.Length; i++ )
+			for (int i = 0; i < m_Entries.Length; i++)
 			{
-				m_Artifacts[i] = new StealableInstance( m_Entries[i] );
+				m_Artifacts[i] = new StealableInstance(m_Entries[i]);
 			}
 
-			m_RespawnTimer = Timer.DelayCall( TimeSpan.Zero, TimeSpan.FromMinutes( 15.0 ), new TimerCallback( CheckRespawn ) );
+			m_RespawnTimer = Timer.DelayCall(TimeSpan.Zero, TimeSpan.FromMinutes(15.0), new TimerCallback(CheckRespawn));
 		}
 
 		public override void OnDelete()
 		{
 			base.OnDelete();
 
-			if ( m_RespawnTimer != null )
+			if (m_RespawnTimer != null)
 			{
 				m_RespawnTimer.Stop();
 				m_RespawnTimer = null;
 			}
 
-			foreach ( StealableInstance si in m_Artifacts )
+			foreach (StealableInstance si in m_Artifacts)
 			{
-				if ( si.Item != null )
+				if (si.Item != null)
 					si.Item.Delete();
 			}
 
@@ -369,66 +473,66 @@ namespace Server.Items
 
 		public void CheckRespawn()
 		{
-			foreach ( StealableInstance si in m_Artifacts )
+			foreach (StealableInstance si in m_Artifacts)
 			{
 				si.CheckRespawn();
 			}
 		}
 
-		public StealableArtifactsSpawner( Serial serial ) : base( serial )
+		public StealableArtifactsSpawner(Serial serial) : base(serial)
 		{
 			m_Instance = this;
 		}
 
-		public override void Serialize( GenericWriter writer )
+		public override void Serialize(GenericWriter writer)
 		{
-			base.Serialize( writer );
+			base.Serialize(writer);
 
-			writer.WriteEncodedInt( 0 ); // version
+			writer.WriteEncodedInt(0); // version
 
-			writer.WriteEncodedInt( m_Artifacts.Length );
+			writer.WriteEncodedInt(m_Artifacts.Length);
 
-			for ( int i = 0; i < m_Artifacts.Length; i++ )
+			for (int i = 0; i < m_Artifacts.Length; i++)
 			{
 				StealableInstance si = m_Artifacts[i];
 
-				writer.Write( (Item) si.Item );
-				writer.WriteDeltaTime( (DateTime) si.NextRespawn );
+				writer.Write((Item)si.Item);
+				writer.WriteDeltaTime((DateTime)si.NextRespawn);
 			}
 		}
 
-		public override void Deserialize( GenericReader reader )
+		public override void Deserialize(GenericReader reader)
 		{
-			base.Deserialize( reader );
+			base.Deserialize(reader);
 
 			int version = reader.ReadEncodedInt();
 
 			m_Artifacts = new StealableInstance[m_Entries.Length];
-			m_Table = new Hashtable( m_Entries.Length );
+			m_Table = new Hashtable(m_Entries.Length);
 
 			int length = reader.ReadEncodedInt();
 
-			for ( int i = 0; i < length; i++ )
+			for (int i = 0; i < length; i++)
 			{
 				Item item = reader.ReadItem();
 				DateTime nextRespawn = reader.ReadDeltaTime();
 
-				if ( i < m_Artifacts.Length )
+				if (i < m_Artifacts.Length)
 				{
-					StealableInstance si = new StealableInstance( m_Entries[i], item, nextRespawn );
+					StealableInstance si = new StealableInstance(m_Entries[i], item, nextRespawn);
 					m_Artifacts[i] = si;
 
-					if ( si.Item != null )
+					if (si.Item != null)
 						m_Table[si.Item] = si;
 				}
 			}
 
-			for ( int i = length; i < m_Entries.Length; i++ )
+			for (int i = length; i < m_Entries.Length; i++)
 			{
-				m_Artifacts[i] = new StealableInstance( m_Entries[i] );
+				m_Artifacts[i] = new StealableInstance(m_Entries[i]);
 			}
 
-			m_RespawnTimer = Timer.DelayCall( TimeSpan.Zero, TimeSpan.FromMinutes( 15.0 ), new TimerCallback( CheckRespawn ) );
+			m_RespawnTimer = Timer.DelayCall(TimeSpan.Zero, TimeSpan.FromMinutes(15.0), new TimerCallback(CheckRespawn));
 		}
 	}
 }
